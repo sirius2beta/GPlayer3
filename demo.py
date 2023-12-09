c6d30a1555785ec1eb346652b1a88121c46239c0
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-12-09 12:06:54 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,8 @@
 import GPlayer
 import DeviceManager
 
-gplayer = GPlayer.GPlayer()
-#gplayer.on_setDevice = sm.setDevice
-#sm.on_message = gplayer.sendMsg
-gplayer.startLoop()
+if __name__ == '__main__':
+  gplayer = GPlayer.GPlayer()
+  #gplayer.on_setDevice = sm.setDevice
+  #sm.on_message = gplayer.sendMsg
+  gplayer.startLoop()
