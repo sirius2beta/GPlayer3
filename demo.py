732b393bd18e502480c9c00a4a8fd4c7fdf3adec
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-12-10 10:13:54 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -9,6 +9,8 @@ if __name__ == '__main__':
 
   
   now = datetime.datetime.now()
+  print("*********************************")
+  print("*********************************")
   print(f"Program started in: {now}")
   gplayer = GPlayer.GPlayer()
   gplayer.startLoop()
